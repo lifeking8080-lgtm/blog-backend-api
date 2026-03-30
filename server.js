7b2631a9b86bcda1ee15dfb011142e7b82823641
Shivraj Shivaji Patil <shivajispatil8968@gmail.com>
2026-03-31 00:36:01 +0530

    Completed server setup and connected all modules

--- server.js
+++ server.js
@@ -6,13 +6,31 @@ const authRoutes = require("./routes/authRoutes");
 const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 
+const User = require("./models/user");
+const Post = require("./models/post");
+const Comment = require("./models/comment");
+
 const app = express();
 app.use(express.json());
 
+// ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 
+// RELATIONSHIPS
+User.hasMany(Post);
+Post.belongsTo(User);
+
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+
+User.hasMany(Comment);
+Comment.belongsTo(User);
+
+// START SERVER
 sequelize.sync().then(() => {
-  app.listen(process.env.PORT, () => console.log("Server running"));
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("Server running 🚀");
+  });
 });
